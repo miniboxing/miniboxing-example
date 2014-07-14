7db5713e9a3d21df9699dc568766f4b2d838c7d8
Vlad Ureche <vlad.ureche@gmail.com>
2014-07-15 00:47:36 +0300

    Version bump

--- project/Build.scala
+++ project/Build.scala
@@ -20,15 +20,13 @@ object MyBuild extends Build {
   /** Settings for the miniboxing plugin */
   lazy val miniboxingSettings = Seq[Setting[_]](
     resolvers += Resolver.sonatypeRepo("snapshots"),
-    libraryDependencies += "org.scala-miniboxing.plugins" %% "miniboxing-runtime" % "0.3-SNAPSHOT",
-    addCompilerPlugin("org.scala-miniboxing.plugins" %% "miniboxing-plugin" % "0.3-SNAPSHOT"),
+    libraryDependencies += "org.scala-miniboxing.plugins" %% "miniboxing-runtime" % "0.4-SNAPSHOT",
+    addCompilerPlugin("org.scala-miniboxing.plugins" %% "miniboxing-plugin" % "0.4-SNAPSHOT"),
     scalacOptions ++= (
       //"-P:minibox:log" ::    // enable the miniboxing plugin output
       //                       // (which explains what the plugin is doing)
       //"-P:minibox:hijack" :: // enable hijacking the @specialized annotations
       //                       // transforming them into @miniboxed annotations
-      //"-P:minibox:two-way" ::// translate to {generic, Long, Double} instead of
-      //                       // just {generic, Long} - further increases performance
       "-optimize" ::         // necessary to get the best performance when
                              // using the miniboxing plugin
       Nil
